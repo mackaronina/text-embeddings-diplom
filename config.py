3f62c994e482103b45d109b59a8dd095d52f74c1
mackaronina <mihamishu123@gmail.com>
2025-05-27 04:15:30 +0300

    Added dotenv

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
 DEVICE = "cuda:0"
 TRAIN_SIZES = [200, 400, 600, 800, 1000]
 BERT_TRAIN_SIZE = 15000
 TEST_SIZE = 2000
 SEED = 1
-BOT_TOKEN = "8005048463:AAHXKLApXCxaiReCw7Q49I7ParVwRt7wNYk"
-DB_URL = "sqlite:///quotes.db"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+DB_URL = os.getenv("DB_URL") or "sqlite:///quotes.db"
